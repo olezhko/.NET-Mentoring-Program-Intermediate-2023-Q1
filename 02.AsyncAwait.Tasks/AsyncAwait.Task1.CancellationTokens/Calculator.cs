ddaba50f16e8eb60ea31d5c1b57c1f6932fec18a
Aleh Ladyka <Aleh_Ladyka@epam.com>
2023-04-22 22:18:14 +0300

    Task 2

--- 02.AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ 02.AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait.Task1.CancellationTokens;
 
@@ -15,6 +16,31 @@ internal static class Calculator
             sum = sum + (i + 1);
             Thread.Sleep(10);
         }
+
+        return sum;
+    }
+
+
+    public static async Task<long> CalculateAsync(int n, CancellationToken token)
+    {
+        long sum = 0;
+
+        await Task.Run(async () =>
+        {
+            for (var i = 0; i < n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return sum;
+                }
+
+                // i + 1 is to allow 2147483647 (Max(Int32)) 
+                sum = sum + (i + 1);
+                await Task.Delay(10);
+            }
+
+            return sum;
+        }, token);
 
         return sum;
     }
